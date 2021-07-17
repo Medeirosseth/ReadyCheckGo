e083dc57245d5b73b22b0494079c1ba344799bc4
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-07-17 13:40:28 -0700

    add completed category

--- rcg/src/components/Item.js
+++ rcg/src/components/Item.js
@@ -8,11 +8,22 @@ const Item = ({text, setItems, items, item}) => {
     console.log(item)
   }
 
+  const completeHandler = () => {
+    setItems(items.map((e) => {
+      if(e.id === item.id){
+        return {
+          ...e, completed: !e.completed
+        }
+      }
+      return e;
+    }))
+  }
+
   return (
     <div className="item">
       <li className="todo-item">{text}</li>
       <button>
-        <i className="fas fa-check">CHECK</i>
+        <i onClick={completeHandler} className="fas fa-check">CHECK</i>
       </button>
       <button>
         <i onClick={deleteHandler} className="fas fa-trash">Trash</i>
