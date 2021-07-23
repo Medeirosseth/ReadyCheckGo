b78e8b5e1ed9ac0badd8413a5d6ab8ab79379360
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-07-23 12:51:09 -0700

    WIP: splash page with animation

--- rcg/src/App.js
+++ rcg/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
-import React, {useState, useEffect } from "react";
+import React, {useState, useEffect, useRef } from "react";
+//import { useSpring, animated } from 'react-spring'
 import Form from './components/Form';
 import CheckList from './components/CheckList';
 
 
+
 function App() {
-  
+  // const textOneRef = useRef();
+  // const textOneStyle = useSpring({
+  //   from: {
+  //     opacity:0
+  //   },
+  //   to:{
+  //     opacity: 1
+  //   }
+  // })
+
   const [inputText, setInputText] = useState('');
   const [items, setItems] = useState([]);
   const [status, setStatus] = useState('all')
@@ -13,28 +24,32 @@ function App() {
 
   useEffect(() => {
     filterHandler()
-
   }, [items, status] )
 
 
 
-const filterHandler = () => {
-  switch(status){
-    case "completed":
-      setFilteredItems(items.filter((item) => item.completed === true ))
-      break;
-    case "uncompleted":
-      setFilteredItems(items.filter((item) => item.completed === false))
-      break;
-    default:
-      setFilteredItems(items);
-      break;
+  const filterHandler = () => {
+    switch(status){
+      case "completed":
+        setFilteredItems(items.filter((item) => item.completed === true ))
+        break;
+      case "uncompleted":
+        setFilteredItems(items.filter((item) => item.completed === false))
+        break;
+      default:
+        setFilteredItems(items);
+        break;
+    }
   }
-}
+
+
 
   return (
     <div className="App">
       <header>Testing, Testing, is this thing on?</header>
+      <animated.h1 style={textOneStyle}> Ready </animated.h1>
+      <h1>Check</h1>
+      <h1>Go</h1>
       <Form 
         setInputText={setInputText}
         setItems={setItems}
