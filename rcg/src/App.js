ecbc11d8b53162c4c1e354d85a5bf3898465689c
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-07-26 09:24:57 -0700

    add stylings

--- rcg/src/App.js
+++ rcg/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 //import { useSpring, animated } from 'react-spring'
 import Header from './components/Header';
 import CheckControl from './components/CheckControl';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Header />
       <CheckControl />
+      <Footer />
     </div>
   );
 }
---
+++ rcg/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const Footer= () => {
+  return (
+    <div className="footer"> this is FOOTER</div>
+  )
+}
+
+
+export default Footer;
